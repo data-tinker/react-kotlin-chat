2f490fa282ac2182140f6fda1d814f8029699ac0
Alex Martishin <asmartishin@gmail.com>
2023-11-30 12:46:46 +0100

    Add coroutines support

--- backend/src/main/kotlin/com/ttymonkey/backend/MessageHandler.kt
+++ backend/src/main/kotlin/com/ttymonkey/backend/MessageHandler.kt
@@ -2,6 +2,9 @@ package com.ttymonkey.backend
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.ttymonkey.backend.models.Message
+import kotlinx.coroutines.channels.broadcast
+import kotlinx.coroutines.reactive.awaitFirstOrNull
+import kotlinx.coroutines.reactor.mono
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.socket.WebSocketHandler
 import org.springframework.web.reactive.socket.WebSocketSession
@@ -14,26 +17,25 @@ class MessageHandler : WebSocketHandler {
     private val sessions = ConcurrentHashMap.newKeySet<WebSocketSession>()
     private val objectMapper = jacksonObjectMapper()
 
-    override fun handle(session: WebSocketSession): Mono<Void> {
+    override fun handle(session: WebSocketSession): Mono<Void> = mono {
         sessions.add(session)
         val joinMessage = Message(2, "New user joined")
-        broadcast(joinMessage, session)
+        broadcast(joinMessage)
 
-        val closed = session.receive()
-            .doOnNext { msg -> broadcast(Message(1, msg.payloadAsText), session) }
+        session.receive()
+            .doOnNext { msg -> broadcast(Message(1, msg.payloadAsText)) }
             .doFinally { signalType ->
                 if (signalType == SignalType.ON_COMPLETE) {
                     sessions.remove(session)
                     val disconnectMessage = Message(2, "User disconnected")
-                    broadcast(disconnectMessage, session)
+                    broadcast(disconnectMessage)
                 }
             }
             .then()
-
-        return closed
+            .awaitFirstOrNull()
     }
 
-    private fun broadcast(message: Message, sender: WebSocketSession) {
+    private fun broadcast(message: Message) {
         val jsonMessage = objectMapper.writeValueAsString(message)
         sessions.forEach { session ->
             session.send(Mono.just(session.textMessage(jsonMessage))).subscribe()
